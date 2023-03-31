a8fd37b4603cd519c9cee851ff45afe6c18cab9c
wooseok-AI <peterjung95@gmail.com>
2023-04-01 02:55:11 +0900

    solved 5650 SWE

--- SWE/5650_핀볼게임.py
+++ SWE/5650_핀볼게임.py
@@ -1,29 +1,71 @@
-import sys
+def solution(pinball_map: list, sr: int, sc: int, d: int):
+    # 블럭 정의
+    # 공의 방향 [북 동 남 서]
+    # 북 0 동 1 남 2 서 3
+    # 1번 블럭  [남쪽으로 전환, 서쪽으로 전환, 동쪽으로 전환, 북쪽으로 전환]
+    # 2번 블럭  [동쪽으로 전환, 서쪽으로 전환, 북쪽으로 전환, 남쪽으로 전환]
+    # 3번 블럭  [서쪽으로 전환, 남쪽으로 전환, 북쪽으로 전환, 동쪽으로 전환]
+    # 4번 블럭  [남쪽으로 전환, 북쪽으로 전환, 서쪽으로 전환, 동쪽으로 전환]
+    # 5번 블럭  [남쪽으로 이동, 서쪽으로 이동, 북쪽으로 이동, 동쪽으로 이동]
+
+    d_col = [0, 1, 0, -1]
+    d_row = [-1, 0, 1, 0]
+    block = {1: [2, 3, 1, 0],
+             2: [1, 3, 0, 2],
+             3: [3, 2, 0, 1],
+             4: [2, 0, 3, 1],
+             5: [2, 3, 0, 1]
+             }
+    # 블랙홀 위치, 웜홀 위치 저장
+    global hole
+    map_size = len(pinball_map)
+    pinball_score = 0
+    move = 0
+    row, col = sr, sc
+
+    while True:
+        row += d_row[d]
+        col += d_col[d]
+        info = pinball_map[row][col]
+
+        if info == -1 or [row, col] == [sr, sc]:
+            break
+        elif info >= 6:
+            row, col = hole[(row, col)]
+        elif 1 <= info <= 5:
+            d = block[info][d]
+            pinball_score += 1
+
+    return pinball_score
 
 
-'''
-      아래의 구문은 input.txt 를 read only 형식으로 연 후,
-      앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
-      여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
-      아래 구문을 이용하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+T = int(input())
+for tc in range(T):
+    N = int(input())
+    pinball_map = []
+    hole_index = [0] * 11
+    hole = dict()
+    pinball_map.append([5] * (N+2))
+    for r in range(N):
+        R = list(map(int, input().split()))
+        pinball_map.append([5] + R + [5])
+        for c in range(N):
+            num = R[c]
+            if 6 <= num <= 10:
+                if not hole_index[num]:
+                    hole_index[num] = (r+1, c+1)
+                else:
+                    hole[(r+1, c+1)] = hole_index[num]
+                    hole[hole_index[num]] = (r+1, c+1)
+    pinball_map.append([5] * (N+2))
 
-      따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 구문을 사용하셔도 좋습니다.
-      아래 구문을 사용하기 위해서는 import sys가 필요합니다.
+    best_score = -1
+    for sr in range(1, N+1):
+        for sc in range(1, N+1):
+            if pinball_map[sr][sc] == 0:
+                for d in range(4):
+                    score = solution(pinball_map, sr, sc, d)
+                    best_score = max(best_score, score)
+    print("#{} {}".format(tc+1, best_score))
 
-      단, 채점을 위해 코드를 제출하실 때에는 반드시 아래 구문을 지우거나 주석 처리 하셔야 합니다.
-'''
-
-
-
-if __name__ == "__main__":
-
-    sys.stdin = open("5650_sample_input.txt", "r")
-
-    for test_case in range(1, T + 1):
-        # ///////////////////////////////////////////////////////////////////////////////////
-        '''
-    
-            이 부분에 여러분의 알고리즘 구현이 들어갑니다.
-
-        '''
-        # ///////////////////////////////////////////////////////////////////////////////////
+    # print(solution(pinball_map, 6, 3, 3))
