dcd7fed43f697088cc10bad0ece1f4a8ce502729
wooseok-AI <peterjung95@gmail.com>
2023-04-03 23:39:23 +0900

    Added DFS solution

--- BOJ/14501_퇴사.py
+++ BOJ/14501_퇴사.py
@@ -1,15 +1,40 @@
 n = int(input())
 schedule = [list(map(int, input().split())) for _ in range(n)]
 
-dp = [0] * (n + 1)
+def dp_solution(N, schedule):
+    dp = [0] * (n + 1)
+
+    for i in range(n-1, -1, -1):
+        t, p = schedule[i]
+        if i + t <= n:
+            dp[i] = max(p + dp[i + t], dp[i + 1])
+        else:
+            dp[i] = dp[i + 1]
+
+    print(dp[0])
 
 
-for i in range(n-1, -1, -1):
-    t, p = schedule[i]
-    if i + t <= n:
-        dp[i] = max(p + dp[i + t], dp[i + 1])
-    else:
-        dp[i] = dp[i + 1]
+def dfs_solution(day, profit):
+    global maximum_profit
+
+    if day == n:
+        maximum_profit = max(maximum_profit, profit)
+        return
+
+    if day + schedule[day][0] <= n:
+        dfs_solution(day+schedule[day][0], profit + schedule[day][1])
+
+    dfs_solution(day+1, profit)
 
 
-print(dp[0])
+maximum_profit = 0
+
+for day in range(n):
+    dfs_solution(day, 0)
+
+print(maximum_profit)
+
+
+
+
+
