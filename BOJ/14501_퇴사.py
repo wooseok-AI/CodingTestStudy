ee092ec01512f39beaa4d381a5bddc4bc641326f
wooseok-AI <peterjung95@gmail.com>
2023-04-03 15:59:45 +0900

    solved 14501 BOJ. Need to be reviewed.

--- BOJ/14501_퇴사.py
+++ BOJ/14501_퇴사.py
@@ -3,11 +3,13 @@ schedule = [list(map(int, input().split())) for _ in range(n)]
 
 dp = [0] * (n + 1)
 
+
 for i in range(n-1, -1, -1):
     t, p = schedule[i]
     if i + t <= n:
         dp[i] = max(p + dp[i + t], dp[i + 1])
     else:
         dp[i] = dp[i + 1]
+
 
 print(dp[0])
