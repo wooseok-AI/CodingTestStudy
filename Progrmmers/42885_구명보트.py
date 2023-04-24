910b1a47843f319ce635e619fc20483cbe7d83f3
wooseok-AI <peterjung95@gmail.com>
2023-04-24 12:02:20 +0900

    Solved Programers 42885

---
+++ Progrmmers/42885_구명보트.py
@@ -0,0 +1,25 @@
+"""
+Author :  Wooseok Jung
+Problem_ling : https://school.programmers.co.kr/learn/courses/30/lessons/42885
+"""
+
+
+def solution(people, limit):
+    people.sort()
+
+    count = 0
+    minimum = 0
+    maximum = len(people) - 1
+
+    while minimum <= maximum:
+        if people[maximum] + people[minimum] <= limit:
+            minimum += 1
+        count += 1
+        maximum -= 1
+
+    return count
+
+if __name__ == "__main__":
+    people = list(map(int, input().split()))
+    limit = int(input())
+    print(solution(people, limit))
